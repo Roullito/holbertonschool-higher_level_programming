11e38c4b7fedb47e8c4cebe5d5a5f478215477a7
Roullito <jmoleins@laposte.net>
2025-05-08 12:25:01 +0000

    program that prints the result of the addition of all arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""
+Script that adds all command-line arguments passed to it.
+
+Usage:
+    ./3-infinite_add.py arg1 arg2 arg3 ...
+Each argument is expected to be an integer.
+"""
 import sys
 
 if __name__ == "__main__":
-    total = 0
+    add_number = 0
     for i in range(1, len(sys.argv)):
-        total += int(sys.argv[i])
-    print("{}".format(total))
-
+        add_number += int(sys.argv[i])
+    print("{}".format(add_number))
