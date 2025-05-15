ea03cf7e222a27bccc97c26afed1a2e8299c4254
Roullito <jmoleins@laposte.net>
2025-05-15 09:43:53 +0200

    Add function add_integer with docstring and doctest cases

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+This module provides a function to add two integers.
+"""
+
+def add_integer(a, b=98):
+    """
+    Adds two integers or floats, casting floats to integers first.
+
+    Args:
+        a (int, float): First number.
+        b (int, float, optional): Second number (default is 98).
+
+    Returns:
+        int: The sum of a and b, both casted to integers.
+
+    Raises:
+        TypeError: If a or b is not an int or float.
+
+    Examples:
+        >>> add_integer(1, 2)
+        3
+        >>> add_integer(100, -2)
+        98
+        >>> add_integer(2)
+        100
+        >>> add_integer(100.3, -2)
+        98
+        >>> add_integer(4, "School")
+        Traceback (most recent call last):
+        ...
+        TypeError: b must be an integer
+        >>> add_integer(None)
+        Traceback (most recent call last):
+        ...
+        TypeError: a must be an integer
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
+
+
+
+
+
