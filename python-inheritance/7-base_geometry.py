b700af65b76aa66d74f96e2a52d34064a4f369a2
Roullito <jmoleins@laposte.net>
2025-05-26 15:10:09 +0200

    fix: pycodestyle

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module that defines BaseGeometry class with area and integer_validator methods."""
+"""Module that defines BaseGeometry class with area and
+integer_validator methods."""
 
 
 class BaseGeometry:
