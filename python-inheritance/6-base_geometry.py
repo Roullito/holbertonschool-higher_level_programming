6c28196030013688e5789f59a1bcbd9167d180cb
Roullito <jmoleins@laposte.net>
2025-05-26 11:13:30 +0200

    Add BaseGeometry.area method raising Exception as placeholder

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Base class for geometry objects."""
+
+    def area(self):
+        """Placeholder method for area computation. To be implemented by subclasses."""
+        raise Exception("area() is not implemented")
