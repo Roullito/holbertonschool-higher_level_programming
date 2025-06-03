34cc585f4013b22285ae395cc632ab20ccd0c0a2
Roullito <jmoleins@laposte.net>
2025-06-03 14:17:27 +0200

    Add Student class with to_json method supporting optional attribute filtering

---
+++ python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            empty_dict = {}
+            for names in attrs:
+                if names in self.__dict__:
+                    empty_dict[names] = self.__dict__[names]
+            return empty_dict
+        else:
+            return self.__dict__
