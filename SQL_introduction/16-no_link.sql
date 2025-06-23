25d4e09d5d7dea699fc1cb8f223f5eb7598c76c5
Roullito <jmoleins@laposte.net>
2025-06-23 15:00:20 +0200

    fix: task 0 by add comment and 16 by add != ''

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,1 +1,2 @@
-script that lists all records of the table second_table of the database hbtn_0c_0
+-- Lists records from second_table where name is not empty, ordered by score descending
+SELECT score, name FROM second_table WHERE name != '' ORDER BY score DESC;
