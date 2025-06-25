af9da3b44730d4f9aafc52ac1f3341fe0955fff7
Roullito <jmoleins@laposte.net>
2025-06-25 11:59:06 +0200

    refactor(script): move database connection logic inside main guard for better structure

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,20 @@
 #!/usr/bin/env python3
-
-"""
-Script that lists all states from the database hbtn_0e_0_usa,
-sorted by id ascending.
-Takes 3 arguments: mysql username, mysql password, and database name.
-"""
-
-
+"""Lists all states from the database hbtn_0e_0_usa, sorted by id ascending."""
 import MySQLdb
 import sys
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=database
-)
-
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-for row in cur.fetchall():
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
