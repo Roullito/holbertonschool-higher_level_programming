3674bed1faf079747bac395418be4edac1dc3cea
Roullito <jmoleins@laposte.net>
2025-06-25 11:57:03 +0200

    refactor(script): streamline argument handling by removing unnecessary main guard

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,23 +10,21 @@ Takes 3 arguments: mysql username, mysql password, and database name.
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+conn = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=username,
+    passwd=password,
+    db=database
+)
 
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    for row in cur.fetchall():
-        print(row)
-    cur.close()
-    conn.close()
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+for row in cur.fetchall():
+    print(row)
+cur.close()
+conn.close()
