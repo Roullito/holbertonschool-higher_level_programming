7b02db18099b4a44d2fdc4dc00bfd1c243351a84
Roullito <jmoleins@laposte.net>
2025-06-26 15:50:57 +0200

    fix(script): remove unnecessary variable assignment and add ORDER BY clause in SQL query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,9 +15,8 @@ if __name__ == "__main__":
         db=sys.argv[3],
     )
     cur = db.cursor()
-    name = sys.argv[4]
     cur.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(name))
+        "SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
