8738a20d8873f37f173db9b1ef0d5194eb3eb5c4
Roullito <jmoleins@laposte.net>
2025-06-26 13:56:15 +0200

    fix(script): update shebang and docstring for clarity in cities listing script

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
-"""Lists all states with a name  matches the argument
-from the database hbtn_0e_0_usa."""
+#!/usr/bin/python3
+"""Lists all cities from the database
+hbtn_0e_0_usa, with their state name."""
 
 
 import MySQLdb
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;")
+        "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
