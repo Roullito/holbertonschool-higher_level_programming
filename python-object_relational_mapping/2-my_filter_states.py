c64d9e9750e372414696f26c7d9b0f04ebd2b973
Roullito <jmoleins@laposte.net>
2025-06-26 15:51:53 +0200

    fix(script): enforce case sensitivity in state name query by using BINARY

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
+        "SELECT * FROM states WHERE BINARY name = '{}'".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
