d00ee23c68cfaee0ff4c8c9ff284bddf8c45a0d2
Roullito <jmoleins@laposte.net>
2025-07-10 13:54:15 +0200

    fix(script): standardize semicolons in 7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,9 +4,9 @@ const argv = 'C is fun';
 const argc = process.argv[2];
 
 if (isNaN(parseInt(argc))) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < parseInt(argc); i++) {
-    console.log(argv)
+    console.log(argv);
   }
 }
