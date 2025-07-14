992bf2530f6231ebe1401022f8a8bb3eaa63a61a
Roullito <jmoleins@laposte.net>
2025-07-14 10:48:55 +0200

    feat(html): add initial HTML structure; feat(script): implement header color toggle functionality

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,16 @@
+const header = document.querySelector("header");
+const toggle = document.querySelector("#toggle_header");
+
+toggle.addEventListener("click", () => {
+    if (header.classList.contains("green")) {
+        header.classList.remove("green");
+        header.classList.add("red");
+    }
+    else if (header.classList.contains("red")) {
+        header.classList.remove("red");
+        header.classList.add("green");
+    }
+    else {
+        header.classList.add("red");
+    }
+});
