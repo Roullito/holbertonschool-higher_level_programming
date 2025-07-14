b9cb85210943d731c0cab172f2c3d41092912765
Roullito <jmoleins@laposte.net>
2025-07-14 14:19:52 +0200

    feat(html): add main HTML structure; feat(script): implement fetch to display greeting message

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("https://hellosalut.stefanbohacek.dev/?lang=fr")
+    .then(response => response.json())
+    .then(data => {
+      const helloDiv = document.querySelector("#hello");
+      helloDiv.textContent = data.hello;
+    })
+    .catch(error => console.error("Erreur :", error));
+});
